5bde84d2cc6968d4bc5d8549728ce5f5b2afb26a
diogo <diogomedina63@gmail.com>
2026-08-25 16:14:54 -0300

    footer e header feitos

---
+++ Front_end/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import './App.css'
+
+function App() {
+  return(
+<>
+  <div className="app">
+    <Header/>
+      <main>
+        
+
+
+
+
+        
+      </main>
+    <Footer />
+  </div>
+</> )
+}
+
+export default App
