ae73992558e8f62dca4a8c08c80f4248094156af
Eduardo Kern Goes da Silva <edugoes215@gmail.com>
2026-08-26 16:37:32 -0300

    Revert "Add files via upload"
    This reverts commit db34f3fd2c023dc4ed59cd7a3711106649d14de2.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import { useState } from 'react'
-import Footer from './components/footer/Footer'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import Sobre from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import CreateAccount from './pages/CreateAccount/CreateAccount';
+import Login from './pages/Login/Login';
 
 function App() {
-  return(
-<>
-  <div className="app">
-    <Header/>
-      <main>
-        
-
-
-
-
-        
-      </main>
-    <Footer />
-  </div>
-</> )
+  return (
+    <Routes>
+      <Route path="/" element={<><Header /><Home /></>} />
+      <Route path="/sobre" element={<><Header /><Sobre /></>} />
+      <Route path="/contato" element={<><Header /><Contact /></>} />
+      <Route path="/criar-conta" element={<CreateAccount />} />
+      <Route path="/entrar" element={<Login />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
